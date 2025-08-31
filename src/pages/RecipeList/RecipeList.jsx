a538f29a81dc1b496dc1255fa8ae1171cfcb8f88
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-01 00:15:41 +0530

    Imported search bar for recipe list page

---
+++ src/pages/RecipeList/RecipeList.jsx
@@ -0,0 +1,8 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
+export default function RecipeList() {
+  return (
+    <div className="flex justify-center flex-col items-center h-screen">
+      <SearchBar />
+    </div>
+  );
+}
