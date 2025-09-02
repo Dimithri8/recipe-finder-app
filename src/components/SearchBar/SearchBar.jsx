a6dd6db3eec098e917958658d642e4cfe840905b
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-03 00:23:30 +0530

    Passed props to search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,25 @@
-export default function SearchBar() {
+import { Box } from "@mui/material";
+
+export default function SearchBar({ handleSubmit, handleChange }) {
   return (
-    <div>
-      <input
-        className="outline-solid outline-[#d1d1d1] outline-sm outline-1 rounded-sm p-2"
-        type="text"
-        placeholder="Search recipe"
-      />
-      <button
-        type="search"
-        className="px-5 py-2 bg-[#f5602a] rounded-sm text-white ml-4"
-      >
-        Search
-      </button>
-    </div>
+    <Box>
+      {" "}
+      <Box component={"form"} onSubmit={handleSubmit}>
+        <input
+          className="outline-solid outline-[#d1d1d1] outline-sm outline-1 rounded-sm p-2"
+          type="text"
+          placeholder="Search recipe"
+          name="search"
+          onChange={handleChange}
+        />
+
+        <button
+          type="submit"
+          className="px-5 py-2 bg-[#f5602a] rounded-sm text-white ml-4"
+        >
+          Search
+        </button>
+      </Box>
+    </Box>
   );
 }
