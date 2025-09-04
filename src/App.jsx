f10ac50957cc84ae54b76dd4e792cc6606e30896
Dimithri8 <dimithridananjaya924@gmail.com>
2025-09-05 00:45:30 +0530

    Setup page routes for 'recipe details' page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import RecipeList from "./pages/RecipeList/RecipeList";
+import RecipeDetails from "./pages/RecipeDetails/RecipeDetails";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<RecipeList />} />
+      <Route
+        path="/recipe-details/:idMeal/:strMeal"
+        element={<RecipeDetails />}
+      />
     </Routes>
   );
 }
